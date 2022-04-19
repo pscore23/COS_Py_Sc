5c9947dffab61e27a0936cd298308f4ffa44eadd
pscore23 <103930835+pscore23@users.noreply.github.com>
2022-04-19 21:01:06 +0900

    Add files via upload

---
+++ COS_Py_Sc.py
@@ -0,0 +1,25 @@
+import scratchconnect # 諸々インポート
+import time
+import datetime
+
+session = scratchconnect.ScratchConnect('_____', '_____') # サインイン
+wait = lambda second: time.sleep(second) # PEP8 は無視していくスタイル
+setwork = lambda text: session.set_work(content=text)
+
+while True: # メインループ
+    while True: # do while 文の再現
+        session.update_data() # 情報を更新
+        msg = session.messages_count() # 未読メッセージ数を取得
+        if (msg > 0):
+            break
+        wait(1)
+    while True: # かなり無理矢理なコードですがお許しを
+        session.update_data()
+        msg = session.messages_count()
+        if (msg == 0):
+            break
+        wait(1)
+    dt = datetime.datetime.now() # 現在日時を取得
+    dt = str(dt.replace(microsecond = 0)) # ミリ秒以下を切り捨て
+    setwork(f'生存確認 ({dt})\r\nこの表示は [PIS3.exe] によって自動更新されています。\r\nインスピレーションをくれた @yukku さんと\r\nこのシステムの開発者の @Rubik_13 さんに感謝!')
+    wait(3)
